064fce5df76f53c0d6a5e769afa819169cdf70c4
angelo-oviedo <angelooviedomont22@gmail.com>
2023-11-08 14:19:21 -0600

    Add fuel data retrieval and car loan payment calculation functions

--- src/calculator.py
+++ src/calculator.py
@@ -9,6 +9,7 @@ import statistics
 #     - Driving frequency on weekdays and weekends.
 
 def getFuelData(url, fuel_type):
+    
     try:
         # Start a session for efficient network calls
         with requests.Session() as session:
@@ -52,3 +53,32 @@ def calculateMonthlyFuelCost(fuel_type, MPG, avg_dist_per_weekday, avg_dist_per_
         return approx_monthly_fuel_cost
     else:
         return "Could not calculate fuel cost due to missing data."
+
+# Car loan and interest rate calculations:
+#     - Monthly cost calculation including down payment consideration.
+
+def calculateMonthlyLoanPayments(loan_amount, down_payment, annual_interest_rate, loan_term_years):
+    """
+    Calculate the monthly payment of a loan.
+
+    Args:
+        loan_amount (float): Total amount of the loan.
+        down_payment (float): Down payment subtracted from the loan amount.
+        annual_interest_rate (float): The annual interest rate (as a decimal, so 5% should be input as 0.05).
+        loan_term_years (int): The length of the loan term in years.
+
+    Returns:
+        float: The monthly payment amount.
+    """
+    principal = loan_amount - down_payment
+    monthly_interest_rate = annual_interest_rate / 12
+    number_of_payments = loan_term_years * 12
+    
+    # If the interest rate is 0, return the principal divided by the number of payments
+    if annual_interest_rate == 0:
+        return principal / number_of_payments
+    
+    # Monthly payment calculation using the formula: https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
+    monthly_payment = principal * (monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments) / ((1 + monthly_interest_rate) ** number_of_payments - 1)
+    
+    return monthly_payment
